ffaa8786151b510c3a27416553ad011b4605fa19
Samuel Attard <sattard@slack-corp.com>
2019-03-21 20:04:24 -0700

    fix: RFH->ExecuteJavascript now requires a callback
    Refs: https://chromium-review.googlesource.com/c/chromium/src/+/1492042

--- atom/browser/ui/inspectable_web_contents_impl.cc
+++ atom/browser/ui/inspectable_web_contents_impl.cc
@@ -410,7 +410,7 @@ void InspectableWebContentsImpl::CallClientFunction(
   }
   javascript.append(");");
   GetDevToolsWebContents()->GetMainFrame()->ExecuteJavaScript(
-      base::UTF8ToUTF16(javascript));
+      base::UTF8ToUTF16(javascript), base::NullCallback());
 }
 
 gfx::Rect InspectableWebContentsImpl::GetDevToolsBounds() const {
@@ -457,7 +457,8 @@ void InspectableWebContentsImpl::LoadCompleted() {
     }
     base::string16 javascript = base::UTF8ToUTF16(
         "Components.dockController.setDockSide(\"" + dock_state_ + "\");");
-    GetDevToolsWebContents()->GetMainFrame()->ExecuteJavaScript(javascript);
+    GetDevToolsWebContents()->GetMainFrame()->ExecuteJavaScript(
+        javascript, base::NullCallback());
   }
 
   if (view_->GetDelegate())
@@ -719,7 +720,8 @@ void InspectableWebContentsImpl::DispatchProtocolMessage(
   if (message.length() < kMaxMessageChunkSize) {
     base::string16 javascript =
         base::UTF8ToUTF16("DevToolsAPI.dispatchMessage(" + message + ");");
-    GetDevToolsWebContents()->GetMainFrame()->ExecuteJavaScript(javascript);
+    GetDevToolsWebContents()->GetMainFrame()->ExecuteJavaScript(
+        javascript, base::NullCallback());
     return;
   }
 
